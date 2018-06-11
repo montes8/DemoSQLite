02069bd951828fb6cf68906a77963ee6e5d90d2c
emontesinos812 <emontesinos812@gmail.com>
2018-06-11 18:47:38 -0300

    agregando botones

--- app/src/main/java/com/example/eddymontesinos/preojectlogin/MainActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/preojectlogin/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.eddymontesinos.preojectlogin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.graphics.Typeface
+import android.text.Spanned
+import android.text.style.StyleSpan
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configurarTextoRegistrate()
+    }
+
+    fun configurarTextoRegistrate(){
+        val string = getString(R.string.registrate)
+
+        val spannableStringBuilder = SpannableStringBuilder(string)
+
+        val boldSpan = StyleSpan(Typeface.BOLD)
+
+        spannableStringBuilder.setSpan(boldSpan, 13, string.length,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvRegistrate.text = spannableStringBuilder
     }
 }
