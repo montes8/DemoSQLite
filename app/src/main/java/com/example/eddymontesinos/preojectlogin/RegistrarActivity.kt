7caadbb1f6ac8194d5ca37300cc74fa264cadc2f
emontesinos812 <emontesinos812@gmail.com>
2018-06-13 11:07:47 -0300

    agregando Dialog Opciones2

--- app/src/main/java/com/example/eddymontesinos/preojectlogin/RegistrarActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/preojectlogin/RegistrarActivity.kt
@@ -2,10 +2,14 @@ package com.example.eddymontesinos.preojectlogin
 
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
 import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_registrar.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -30,10 +34,32 @@ class RegistrarActivity : AppCompatActivity() {
     }
 
     private fun cargarImagen() {
-        val intent:Intent = Intent(Intent.ACTION_PICK,android.provider
-                           .MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.type = "image/"
-        startActivityForResult(intent,10)
+        //metodo simple cargaimagenes de galeria
+        //val intent:Intent = Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        val opciones = arrayOf<CharSequence>("Tomar foto", "Cargar imagen", "Cancelar")
+        val alertOpciones = AlertDialog.Builder(this@RegistrarActivity)
+        alertOpciones.setTitle("Seleccione una Opcion")
+        alertOpciones.setItems(opciones) { dialog, i ->
+            if (opciones[i] == "Tomar foto") {
+
+                Toast.makeText(application, "tomar foto", Toast.LENGTH_LONG).show()
+
+            } else {
+                if (opciones[i] == "Cargar imagen") {
+
+                    val intent:Intent = Intent(Intent.ACTION_GET_CONTENT,android.provider
+                            .MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    intent.type = "image/"
+                    startActivityForResult(intent,10)
+                } else {
+                    dialog.dismiss()
+                }
+            }
+        }
+        alertOpciones.show()
+
+
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
